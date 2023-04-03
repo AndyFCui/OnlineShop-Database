a7207a1d8918c1d6d8292a352e97d9d84bac4e56
Silow <99187811+Silow9@users.noreply.github.com>
2023-04-03 19:04:02 -0400

    delete customer user id and password

--- RBprojectCREATE.sql
+++ RBprojectCREATE.sql
@@ -1,7 +1,8 @@
 DROP DATABASE IF EXISTS rbstore;
 CREATE DATABASE rbstore;
 USE rbstore;
-CREATE TABLE Operator
+DROP TABLE IF EXISTS operator;
+CREATE TABLE operator
 (
 	operator_id int primary key,
     name varchar(50) not null,
@@ -12,20 +13,18 @@ CREATE TABLE Operator
     user_id int not null,
     user_password int not null
 );
-
-CREATE TABLE Customer
+DROP TABLE IF EXISTS customer;
+CREATE TABLE customer
 (
 	customer_id int primary key,
     name varchar(50) not null,
     address varchar(50) not null,
     phone_number int not null,
     legal_sex varchar (50) not null,
-    date_of_birth date not null,
-    user_id int not null,
-    user_password int not null
+    date_of_birth date not null
 );
-
-CREATE TABLE RBorder
+DROP TABLE IF EXISTS robot_order;
+CREATE TABLE robot_order
 (
 	order_id int primary key,
     order_date date not null,
@@ -34,36 +33,36 @@ CREATE TABLE RBorder
     deliver_preference varchar(50) default 'ship',
     operator_id int not null,
     customer_id int not null,
-	CONSTRAINT order_foreign_customer FOREIGN KEY (customer_id) REFERENCES Customer(customer_id)
+	CONSTRAINT order_foreign_customer FOREIGN KEY (customer_id) REFERENCES customer(customer_id)
     ON DELETE CASCADE ON UPDATE RESTRICT,    
-	CONSTRAINT order_foreign_operator FOREIGN KEY (operator_id) REFERENCES Operator(operator_id)
+	CONSTRAINT order_foreign_operator FOREIGN KEY (operator_id) REFERENCES operator(operator_id)
     ON DELETE CASCADE ON UPDATE RESTRICT
 );
-
-CREATE TABLE Credit_card
+DROP TABLE IF EXISTS credit_card;
+CREATE TABLE credit_card
 (
 	card_number int primary key,
     type varchar(50) not null,
     expiration_date date not null,
     customer_id int not null,
-    CONSTRAINT card_foreign_customer FOREIGN KEY (customer_id) REFERENCES Customer(customer_id)
+    CONSTRAINT card_foreign_customer FOREIGN KEY (customer_id) REFERENCES customer(customer_id)
     ON DELETE CASCADE ON UPDATE RESTRICT
 );
-
-CREATE TABLE Software_edition
+DROP TABLE IF EXISTS credit_card;
+CREATE TABLE software_edition
 (
 	edition varchar(50) primary key,
     description varchar(50) not null,
     release_date date not null
 );
-
-CREATE TABLE Rb_model
+DROP TABLE IF EXISTS robot_model;
+CREATE TABLE robot_model
 (
 	model_id int primary key,
     model_name varchar(50) not null
 );
-
-CREATE TABLE Robot
+DROP TABLE IF EXISTS robot;
+CREATE TABLE robot
 (
 	goods_id int primary key,
     instock varchar(50) not null,
@@ -71,20 +70,28 @@ CREATE TABLE Robot
     software_edition varchar(50) default '0',
     purchased_cost int not null,
     model_id int not null,
-    CONSTRAINT rbot_foreign_software FOREIGN KEY (software_edition) REFERENCES Software_edition(edition)
+    CONSTRAINT rbot_foreign_software FOREIGN KEY (software_edition) REFERENCES software_edition(edition)
     ON DELETE CASCADE ON UPDATE RESTRICT,
-    CONSTRAINT rbot_foreign_model FOREIGN KEY (model_id) REFERENCES Rbot_model(model_id)
+    CONSTRAINT rbot_foreign_model FOREIGN KEY (model_id) REFERENCES robot_model(model_id)
     ON DELETE CASCADE ON UPDATE RESTRICT
 );
-
-CREATE TABLE Order_detail
+DROP TABLE IF EXISTS order_detail;
+CREATE TABLE order_detail
 (
 	order_id int not null,
     goods_id int not null,
     sales_price int not null,
     primary key(order_id, goods_id),
-    CONSTRAINT detail_foreign_order FOREIGN KEY (order_id) REFERENCES RBorder(order_id)
+    CONSTRAINT detail_foreign_order FOREIGN KEY (order_id) REFERENCES robot_order(order_id)
     ON DELETE CASCADE ON UPDATE RESTRICT,
-	CONSTRAINT detail_foreign_good FOREIGN KEY (goods_id) REFERENCES Robot(goods_id)
+	CONSTRAINT detail_foreign_good FOREIGN KEY (goods_id) REFERENCES robot(goods_id)
     ON DELETE CASCADE ON UPDATE RESTRICT
 );
+
+
+
+
+
+
+
+
