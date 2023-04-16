56d9fbda4bc2ddbcd4b029b5956d8d98048e3420
Silow <99187811+Silow9@users.noreply.github.com>
2023-04-16 19:27:49 -0400

    test finished

--- operator_sys/res/procedure/p_return.sql
+++ operator_sys/res/procedure/p_return.sql
@@ -3,15 +3,21 @@ use rbstore;
 -- return payment
 DROP PROCEDURE IF EXISTS return_payment;
 DELIMITER  //
-CREATE PROCEDURE return_confirm(
+CREATE PROCEDURE return_payment(
 	IN return_order_id int,
     IN return_goods_id int
 )
 BEGIN
-	UPDATE order_detail SET return_status = "payment return" where order_id = return_id and goods_id = return_goods_id;
+	UPDATE order_detail SET return_status = "payment return" where order_id = return_order_id and goods_id = return_goods_id;
     UPDATE robot_order SET order_status = "has goods to return" WHERE order_id = return_order_id;
 END//
 DELIMITER ;
+-- TEST
+-- CALL return_payment(1,4000);
+-- DELIMITER  //
+-- SELECT * FROM order_detail;
+-- SELECT * FROM robot_order//
+-- DELIMITER ;
 
 -- return payment and goods
 DROP PROCEDURE IF EXISTS return_payment_and_goods;
@@ -21,24 +27,35 @@ CREATE PROCEDURE return_payment_and_goods(
     IN return_goods_id int
 )
 BEGIN
-	UPDATE order_detail SET return_status = "payment and goods return" where order_id = return_id and goods_id = return_goods_id;
+	UPDATE order_detail SET return_status = "payment and goods return" where order_id = return_order_id and goods_id = return_goods_id;
     UPDATE robot_order SET order_status = "has goods and payment to return" WHERE order_id = return_order_id;
 END//
 DELIMITER ;
+-- TEST
+-- CALL return_payment_and_goods(1,4000);
+-- DELIMITER  //
+-- SELECT * FROM order_detail;
+-- SELECT * FROM robot_order//
+-- DELIMITER ;
 
 -- exchange
-DROP PROCEDURE IF EXISTS exchange;
+DROP PROCEDURE IF EXISTS return_exchange;
 DELIMITER  //
-CREATE PROCEDURE return_payment_and_goods(
+CREATE PROCEDURE return_exchange(
 	IN return_order_id int,
     IN return_goods_id int
 )
 BEGIN
-	UPDATE order_detail SET return_status = "need to exchange" where order_id = return_id and goods_id = return_goods_id;
+	UPDATE order_detail SET return_status = "need to exchange" where order_id = return_order_id and goods_id = return_goods_id;
     UPDATE robot_order SET order_status = "has goods to exchange" WHERE order_id = return_order_id;
 END//
 DELIMITER ;
-
+-- TEST
+-- CALL return_exchange(1,4000);
+-- DELIMITER  //
+-- SELECT * FROM order_detail;
+-- SELECT * FROM robot_order//
+-- DELIMITER ;
 
 -- return denied
 DROP PROCEDURE IF EXISTS return_denied;
@@ -48,7 +65,32 @@ CREATE PROCEDURE return_denied(
     IN return_goods_id int
 )
 BEGIN
-	UPDATE order_detail SET return_status = "return denied" where order_id = return_id and goods_id = return_goods_id;
+	UPDATE order_detail SET return_status = "return denied" where order_id = return_order_id and goods_id = return_goods_id;
     UPDATE robot_order SET order_status = "return denied" WHERE order_id = return_order_id;    
 END//
 DELIMITER ;
+-- TEST
+-- CALL return_denied(1,4000);
+-- DELIMITER  //
+-- SELECT * FROM order_detail;
+-- SELECT * FROM robot_order//
+-- DELIMITER ;
+
+-- return cancel
+DROP PROCEDURE IF EXISTS return_cancel;
+DELIMITER  //
+CREATE PROCEDURE return_cancel(
+	IN return_order_id int,
+    IN return_goods_id int
+)
+BEGIN
+	UPDATE order_detail SET return_status = "None" where order_id = return_order_id and goods_id = return_goods_id;
+    UPDATE robot_order SET order_status = "Order" WHERE order_id = return_order_id;    
+END//
+DELIMITER ;
+-- TEST
+-- CALL return_cancel(1,4000);
+-- DELIMITER  //
+-- SELECT * FROM order_detail;
+-- SELECT * FROM robot_order//
+-- DELIMITER ;
