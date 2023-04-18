c915188bce28920ed47e80010bcac73258789a6c
Silow <99187811+Silow9@users.noreply.github.com>
2023-04-17 21:50:59 -0400

    add view_order procedure

--- operator_sys/res/procedure/p_order.sql
+++ operator_sys/res/procedure/p_order.sql
@@ -40,3 +40,25 @@ DELIMITER ;
 -- TEST
 -- CALL order_fill(1, 4000, 500);
 -- SELECT sales_price from order_detail where order_id = 1;
+
+-- View order
+DELIMITER  //
+DROP PROCEDURE IF EXISTS view_order_status;
+CREATE PROCEDURE view_order_status(
+	IN in_order_id int,
+    OUT status varchar(50)
+)
+BEGIN
+    SELECT order_status 
+    FROM robot_order 
+    where order_id = in_order_id
+    into status;
+END//
+DELIMITER ;
+-- TEST
+DELIMITER  //
+SET @status = 'None';
+CALL view_order_status(1,@status);
+SELECT @status;
+END//
+DELIMITER ;
