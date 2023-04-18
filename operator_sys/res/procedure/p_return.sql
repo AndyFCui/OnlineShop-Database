8ab9a738d96213c6edef62a5048f9abb30fc6218
Silow <99187811+Silow9@users.noreply.github.com>
2023-04-17 21:33:30 -0400

    update return judgement of date

--- operator_sys/res/procedure/p_return.sql
+++ operator_sys/res/procedure/p_return.sql
@@ -5,30 +5,60 @@ DROP PROCEDURE IF EXISTS return_payment;
 DELIMITER  //
 CREATE PROCEDURE return_payment(
 	IN return_order_id int,
-    IN return_goods_id int
+    IN return_goods_id int,
+    OUT message varchar(50)
 )
 BEGIN
-	UPDATE order_detail SET return_status = "payment return" where order_id = return_order_id and goods_id = return_goods_id;
+	DECLARE nowdate, buy_date date;
+    DECLARE gap int;
+    SELECT CURDATE() into nowdate;
+    SELECT order_date from robot_order where order_id = return_order_id into buy_date;
+    SELECT TIMESTAMPDIFF(DAY, nowdate, buy_date) into gap;
+    IF gap <16 THEN
+    UPDATE order_detail SET return_status = "payment return" where order_id = return_order_id and goods_id = return_goods_id;
     UPDATE robot_order SET order_status = "has goods to return" WHERE order_id = return_order_id;
+	SET message = 'Return request success';
+    ELSE
+	UPDATE order_detail SET return_status = "return denied" where order_id = return_order_id and goods_id = return_goods_id;
+    UPDATE robot_order SET order_status = "return denied" WHERE order_id = return_order_id;
+    SET message = 'Return request expired';
+    END IF;
 END//
 DELIMITER ;
--- TEST
--- CALL return_payment(1,4000);
+-- TEST 
 -- DELIMITER  //
+-- SET @message = 'None';
+-- -- CALL order_create('2023-04-15', 'Order', 'regular', 1, 'Tom');
+-- -- CALL order_fill(2, 3000, 500);
+-- CALL return_payment(1, 4000, @message);
+-- SELECT @message;
 -- SELECT * FROM order_detail;
 -- SELECT * FROM robot_order//
 -- DELIMITER ;
+-- SELECT * from robot;
 
 -- return payment and goods
 DROP PROCEDURE IF EXISTS return_payment_and_goods;
 DELIMITER  //
 CREATE PROCEDURE return_payment_and_goods(
 	IN return_order_id int,
-    IN return_goods_id int
+    IN return_goods_id int,
+    OUT message varchar(50)
 )
 BEGIN
-	UPDATE order_detail SET return_status = "payment and goods return" where order_id = return_order_id and goods_id = return_goods_id;
+	DECLARE nowdate, buy_date date;
+    DECLARE gap int;
+    SELECT CURDATE() into nowdate;
+    SELECT order_date from robot_order where order_id = return_order_id into buy_date;
+    SELECT TIMESTAMPDIFF(DAY, nowdate, buy_date) into gap;
+    IF gap <16 THEN
+    UPDATE order_detail SET return_status = "payment and goods return" where order_id = return_order_id and goods_id = return_goods_id;
     UPDATE robot_order SET order_status = "has goods and payment to return" WHERE order_id = return_order_id;
+    ELSE
+	UPDATE order_detail SET return_status = "return denied" where order_id = return_order_id and goods_id = return_goods_id;
+    UPDATE robot_order SET order_status = "return denied" WHERE order_id = return_order_id;
+    SET message = 'Return request expired';
+    END IF;    
 END//
 DELIMITER ;
 -- TEST
@@ -43,34 +73,27 @@ DROP PROCEDURE IF EXISTS return_exchange;
 DELIMITER  //
 CREATE PROCEDURE return_exchange(
 	IN return_order_id int,
-    IN return_goods_id int
+    IN return_goods_id int,
+    OUT message varchar(50)
 )
 BEGIN
-	UPDATE order_detail SET return_status = "need to exchange" where order_id = return_order_id and goods_id = return_goods_id;
+	DECLARE nowdate, buy_date date;
+    DECLARE gap int;
+    SELECT CURDATE() into nowdate;
+    SELECT order_date from robot_order where order_id = return_order_id into buy_date;
+    SELECT TIMESTAMPDIFF(DAY, nowdate, buy_date) into gap;
+    IF gap <16 THEN
+    UPDATE order_detail SET return_status = "need to exchange" where order_id = return_order_id and goods_id = return_goods_id;
     UPDATE robot_order SET order_status = "has goods to exchange" WHERE order_id = return_order_id;
+    ELSE
+	UPDATE order_detail SET return_status = "return denied" where order_id = return_order_id and goods_id = return_goods_id;
+    UPDATE robot_order SET order_status = "return denied" WHERE order_id = return_order_id;
+    SET message = 'Return request expired';
+    END IF;
 END//
 DELIMITER ;
 -- TEST
 -- CALL return_exchange(1,4000);
--- DELIMITER  //
--- SELECT * FROM order_detail;
--- SELECT * FROM robot_order//
--- DELIMITER ;
-
--- return denied
-DROP PROCEDURE IF EXISTS return_denied;
-DELIMITER  //
-CREATE PROCEDURE return_denied(
-	IN return_order_id int,
-    IN return_goods_id int
-)
-BEGIN
-	UPDATE order_detail SET return_status = "return denied" where order_id = return_order_id and goods_id = return_goods_id;
-    UPDATE robot_order SET order_status = "return denied" WHERE order_id = return_order_id;    
-END//
-DELIMITER ;
--- TEST
--- CALL return_denied(1,4000);
 -- DELIMITER  //
 -- SELECT * FROM order_detail;
 -- SELECT * FROM robot_order//
