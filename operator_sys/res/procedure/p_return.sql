0160cd3d4785b0875e44163ee837349d9fa40142
Silow <99187811+Silow9@users.noreply.github.com>
2023-04-20 22:57:25 -0400

    default final edition

---
+++ operator_sys/res/procedure/p_return.sql
@@ -0,0 +1,103 @@
+use rbstore;
+##########################################################################
+-- return payment
+DROP PROCEDURE IF EXISTS return_payment;
+DELIMITER  //
+CREATE PROCEDURE return_payment(
+	IN return_order_id int,
+    IN return_goods_id int
+)
+BEGIN
+	DECLARE nowdate, buy_date date;
+    DECLARE message varchar(50);
+    DECLARE gap int;
+    SELECT CURDATE() into nowdate;
+    SELECT order_date from robot_order where order_id = return_order_id into buy_date;
+    SELECT TIMESTAMPDIFF(DAY, nowdate, buy_date) into gap;
+    IF gap <16 THEN
+    UPDATE order_detail SET return_status = "payment return" where order_id = return_order_id and goods_id = return_goods_id;
+    UPDATE robot_order SET order_status = "has goods to return" WHERE order_id = return_order_id;
+	SET message = 'Return request success';
+    ELSE
+	UPDATE order_detail SET return_status = "return denied" where order_id = return_order_id and goods_id = return_goods_id;
+    UPDATE robot_order SET order_status = "return denied" WHERE order_id = return_order_id;
+    SET message = 'Return request expired';
+    END IF;
+    SELECT message;
+END//
+DELIMITER ;
+-- TEST 
+-- DELIMITER  //
+-- -- CALL order_create('2023-04-15', 'Order', 'regular', 1, 'Tom');
+-- -- CALL order_fill(2, 3000, 500);
+-- CALL return_payment(1, 4000);
+-- SELECT * FROM order_detail;
+-- SELECT * FROM robot_order//
+-- DELIMITER ;
+-- SELECT * from robot;
+
+-- return payment and goods
+DROP PROCEDURE IF EXISTS return_payment_and_goods;
+DELIMITER  //
+CREATE PROCEDURE return_payment_and_goods(
+	IN return_order_id int,
+    IN return_goods_id int
+)
+BEGIN
+	DECLARE nowdate, buy_date date;
+	DECLARE message varchar(50);
+    DECLARE gap int;
+    SELECT CURDATE() into nowdate;
+    SELECT order_date from robot_order where order_id = return_order_id into buy_date;
+    SELECT TIMESTAMPDIFF(DAY, nowdate, buy_date) into gap;
+    IF gap <16 THEN
+    UPDATE order_detail SET return_status = "payment and goods return" where order_id = return_order_id and goods_id = return_goods_id;
+    UPDATE robot_order SET order_status = "has goods and payment to return" WHERE order_id = return_order_id;
+	SET message = 'Return request success';
+    ELSE
+	UPDATE order_detail SET return_status = "return denied" where order_id = return_order_id and goods_id = return_goods_id;
+    UPDATE robot_order SET order_status = "return denied" WHERE order_id = return_order_id;
+    SET message = 'Return request expired';
+    END IF;
+    SELECT message;
+END//
+DELIMITER ;
+-- TEST
+-- CALL return_payment_and_goods(1,4000);
+-- DELIMITER  //
+-- SELECT * FROM order_detail;
+-- SELECT * FROM robot_order//
+-- DELIMITER ;
+
+-- exchange
+DROP PROCEDURE IF EXISTS return_exchange;
+DELIMITER  //
+CREATE PROCEDURE return_exchange(
+	IN return_order_id int,
+    IN return_goods_id int
+)
+BEGIN
+	DECLARE nowdate, buy_date date;
+	DECLARE message varchar(50);
+    DECLARE gap int;
+    SELECT CURDATE() into nowdate;
+    SELECT order_date from robot_order where order_id = return_order_id into buy_date;
+    SELECT TIMESTAMPDIFF(DAY, nowdate, buy_date) into gap;
+    IF gap <16 THEN
+    UPDATE order_detail SET return_status = "need to exchange" where order_id = return_order_id and goods_id = return_goods_id;
+    UPDATE robot_order SET order_status = "has goods to exchange" WHERE order_id = return_order_id;
+	SET message = 'Return request success';
+    ELSE
+	UPDATE order_detail SET return_status = "return denied" where order_id = return_order_id and goods_id = return_goods_id;
+    UPDATE robot_order SET order_status = "return denied" WHERE order_id = return_order_id;
+    SET message = 'Return request expired';
+    END IF;
+    SELECT message;
+END//
+DELIMITER ;
+-- TEST
+-- CALL return_exchange(1,4000);
+-- DELIMITER  //
+-- SELECT * FROM order_detail;
+-- SELECT * FROM robot_order//
+-- DELIMITER ;
---
+++ operator_sys/res/procedure/p_robot_model.sql
@@ -0,0 +1,57 @@
+use rbstore;
+##########################################################################
+-- insert Robot model
+DROP PROCEDURE IF EXISTS insert_robot_model;
+
+DELIMITER  //
+CREATE PROCEDURE insert_robot_model(
+	IN in_model_id int,
+    IN in_model_name varchar(50)
+    )
+BEGIN
+	DECLARE indicate_model INT;
+    SELECT COUNT(*)
+    FROM robot_model
+    where model_id = in_model_id
+    INTO indicate_model;
+    IF indicate_model <> 0 THEN
+		SIGNAL SQLSTATE '60001' SET MESSAGE_TEXT = 'This model already exists';
+	ELSE
+		INSERT INTO robot_model(model_id, model_name)
+		VALUES (in_model_id, in_model_name);
+	END IF;
+END//
+DELIMITER ;
+-- TEST
+-- CALL insert_robot_model(428910, 'Maru');
+-- SELECT * FROM robot_model;
+##########################################################################
+-- delete model
+DROP PROCEDURE IF EXISTS delete_robot_model;
+DELIMITER  //
+CREATE PROCEDURE delete_robot_model(
+    IN delete_model_id int
+)
+BEGIN
+	DELETE FROM robot_model
+    where model_id = delete_model_id;
+END//
+DELIMITER ;
+-- TEST
+-- CALL delete_robot_model(428910);
+-- SELECT COUNT(*) FROM robot_model where model_id = 428910;
+
+-- View model
+DROP PROCEDURE IF EXISTS view_model;
+DELIMITER  //
+CREATE PROCEDURE view_model(
+	IN in_model_name varchar(50)
+)
+BEGIN
+    SELECT * 
+    FROM robot_model 
+    where model_name = in_model_name;
+END//
+DELIMITER ;
+-- TEST
+-- CALL view_model('Maru');
